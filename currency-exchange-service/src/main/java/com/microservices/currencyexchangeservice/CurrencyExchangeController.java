6a75f4bc2802f13b71bb6da3f43f5b30558cfb4c
Lucas Horiye <lucas2612@gmail.com>
2020-01-18 21:08:19 +0000

    sleuth

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +17,15 @@ public class CurrencyExchangeController {
 	@Autowired
 	private ExchangeValueRepository repository;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
 		ExchangeValue ev = repository.findByFromAndTo(from, to);
 		ev.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		
+		logger.info("{}", ev);
 		return ev;
 		
 	}
